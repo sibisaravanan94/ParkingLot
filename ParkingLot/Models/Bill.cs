666bb2dc531859ea2070c4a2c67a19a4f9378cff
sibisaravanan94_69ee6ac0e1c0-org <sibisaravanan94@gmail.com>
2024-01-05 16:00:38 +0530

    Implemented Bill model

--- ParkingLot/Models/Bill.cs
+++ ParkingLot/Models/Bill.cs
@@ -1,6 +1,13 @@
 ﻿namespace ParkingLot.Models
 {
-    public class Bill
+    public class Bill : BaseModel
     {
+        public string BillID { get; set; }
+        public Ticket Ticket { get; set; }
+        public double Amount { get; set; }
+        public List<Payment> Payments { get; set; } = new List<Payment>();
+        public ParkingGate ExitGate { get; set; }
+        public ParkingOperator Operator { get; set; }
+        public DateTime ExitTime { get; set; }
     }
 }
