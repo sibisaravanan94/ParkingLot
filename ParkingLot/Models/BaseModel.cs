93f7a44e67de62da17bb79ff96010895f02021e8
sibisaravanan94_69ee6ac0e1c0-org <sibisaravanan94@gmail.com>
2024-01-05 07:40:49 +0530

    Created Base model and parking lot model

---
+++ ParkingLot/Models/BaseModel.cs
@@ -0,0 +1,9 @@
+﻿namespace ParkingLot.Models
+{
+    public abstract class BaseModel
+    {
+        public long Id { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+    }
+}
---
+++ ParkingLot/Models/ParkingLot.cs
@@ -0,0 +1,11 @@
+﻿namespace ParkingLot.Models
+{
+    public class ParkingLot : BaseModel
+    {
+        public string Name { get; set; }
+        public string Adress { get; set; }
+        public List<ParkingFloor> ParkingFloors { get; set; } = new List<ParkingFloor>();
+        public List<EntryGate> EntryGates { get; set; } = new List<EntryGate>();
+        public List<ExitGate> ExitGates { get; set; } = new List<ExitGate>();
+    }
+}
